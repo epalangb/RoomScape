d6a790232b101e64e813efce237803f94d4eb5ff
cronopiorz <prabadan@ucm.es>
2021-04-03 17:25:33 +0200

    DAOVideojuego con altavideojuego, falta listar

---
+++ DaoVideojuegoImp.java
@@ -0,0 +1,25 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DaoVideojuegoImp  implements   DAOVideojuego{
+    @Override
+    public void altaVideojuego(TFVideojuego tfVideojuego) {
+        PreparedStatement stmt;
+        try {
+            stmt = DB.getConnection().prepareStatement("INSERT INTO VIDEOJUEGO (nombre, descripcion, consola) VALUES (?,?,?)");
+
+            stmt.setString(1,tfVideojuego.getNombre());
+            stmt.setString(2,tfVideojuego.getDescripcion());
+            stmt.setString(3,tfVideojuego.getConsola());
+            stmt.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    public ArrayList<TFVideojuego> listarVideojuegos() {
+        return null;
+    }
+}
