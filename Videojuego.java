0347842945d5b52af0281292177585a77237ddfc
Kronos808 <jesuga05@ucm.es>
2021-04-03 14:10:04 +0200

    nueva vista del menú principal y del alta de videojuego

---
+++ Videojuego.java
@@ -0,0 +1,17 @@
+
+public class Videojuego {
+
+	
+	private String nombre;
+	private String descripcion;
+	private String consola;
+
+	public Videojuego(String name, String description, String console) {
+		// TODO Auto-generated constructor stub
+		
+		nombre = name;
+		descripcion = description;
+		consola = console;
+	}
+	
+}
