771c6721b63a63a6a98b967c5878ca2d16266bf3
Kronos808 <jesuga05@ucm.es>
2021-04-03 14:15:55 +0200

    transfer videojuego

---
+++ TFVideojuego.java
@@ -0,0 +1,35 @@
+public class TFVideojuego {
+    private String nombre;
+    private String descripcion;
+    private String consola;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getConsola() {
+        return consola;
+    }
+
+    public void setConsola(String consola) {
+        this.consola = consola;
+    }
+
+    public TFVideojuego(String nombre, String descripcion, String consola) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.consola = consola;
+    }
+}
