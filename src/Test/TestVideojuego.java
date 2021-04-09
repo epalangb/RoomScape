c1f0740afe90c06a906ffbdd6720d8ebfa15bec6
Kele-M <miguelmordillo96@gmail.com>
2021-04-09 12:10:02 +0200

    Implementacion de tests v1

---
+++ src/Test/TestVideojuego.java
@@ -0,0 +1,23 @@
+package src.Test;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestVideojuego {
+
+    @BeforeClass
+    public static void beforeClass(){
+
+
+    }
+
+    @AfterClass
+    public static void afterClass(){
+
+
+    }
+}
