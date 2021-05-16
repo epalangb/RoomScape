480164d69aece70ad600d61647e566f7275e8865
Jose Prieto <joseprieto9657@gmail.com>
2021-05-16 13:16:31 +0200

    ROOM 163: Hechos los test de integracion de crear cliente

---
+++ roomscape-backend/src/integration-test/java/roomscape/es/roomscapebackend/negocio/client/SACreateClientTest.java
@@ -0,0 +1,36 @@
+package roomscape.es.roomscapebackend.negocio.client;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SACreateClientTest {
+
+    @Autowired
+    SAClient saClient;
+
+    @Test
+    @DisplayName("Check if the client has been created correctly")
+    public void createClient() {
+
+        TClient tClient = new TClient();
+        tClient.setDni("15978534C");
+        tClient.setUser("Test1");
+        tClient.setPassword("password123roomscape");
+
+        try {
+            TClient tClientSaved = saClient.createClient(tClient);
+            Assertions.assertEquals(tClientSaved.getDni(), tClient.getDni());
+            Assertions.assertEquals(tClientSaved.getUser(), tClient.getUser());
+            Assertions.assertEquals(tClientSaved.getId(), 1);
+            Assertions.assertEquals(tClientSaved.getPassword(), tClient.getPassword());
+            Assertions.assertTrue(tClientSaved.isActive());
+        } catch (Exception e) {
+            Assertions.assertNull(e);
+        }
+    }
+
+}
