bdb7883fabc7f2c53f20ff9e272713cf894da155
MiguelM <miguelmordillo96@gmail.com>
2021-05-18 19:30:44 +0200

    ROOM-166: Añadido al SACliente la nueva operacion

--- roomscape-backend/src/main/java/roomscape/es/roomscapebackend/negocio/client/SAClient.java
+++ roomscape-backend/src/main/java/roomscape/es/roomscapebackend/negocio/client/SAClient.java
@@ -2,4 +2,5 @@ package roomscape.es.roomscapebackend.negocio.client;
 
 public interface SAClient {
     TClient createClient(TClient tClient) throws Exception;
+    boolean clientLogin (Object loginData) throws Exception;
 }
